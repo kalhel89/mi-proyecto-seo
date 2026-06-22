5231df22c93ddd34cfc2cae4f758c826c6d01da1
Kalhel Eithan Gomez Avalos <kalhel.gomez@tecsup.edu.pe>
2026-06-21 23:58:17 -0500

    cambdawwaawdawdwa

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,15 +1,15 @@
 export default function sitemap() {
   return [
     {
-      url: "https://TU-PROYECTO.vercel.app",
+      url: "https://mi-proyecto-seo-two.vercel.app/",
       lastModified: new Date(),
     },
     {
-      url: "https://TU-PROYECTO.vercel.app/blog",
+      url: "https://mi-proyecto-seo-two.vercel.app/blog",
       lastModified: new Date(),
     },
     {
-      url: "https://TU-PROYECTO.vercel.app/contacto",
+      url: "https://mi-proyecto-seo-two.vercel.app/contacto",
       lastModified: new Date(),
     },
   ];

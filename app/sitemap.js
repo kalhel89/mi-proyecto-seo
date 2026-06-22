8ab31e7277587544a799876e846c1b94a6b599ad
Kalhel Eithan Gomez Avalos <kalhel.gomez@tecsup.edu.pe>
2026-06-22 00:08:48 -0500

    sitemap3

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,16 +1,18 @@
 const BASE_URL = "https://mi-proyecto-seo-two.vercel.app/";
 
-export default async function handler(req, res) {
-  const urls = ["/", "/blog", "/contacto"]; // Agrega más rutas dinámicamente
+export default async function sitemap() {
+  const posts = ["post-1", "post-2"];
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls
-      .map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`)
-      .join("")}
-  </urlset>`;
+  const postUrls = posts.map((slug) => ({
+    url: `${BASE_URL}/blog/${slug}`,
+    lastModified: new Date(),
+  }));
 
-  res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
+  return [
+    {
+      url: `${BASE_URL}/`,
+      lastModified: new Date(),
+    },
+    ...postUrls,
+  ];
 }
